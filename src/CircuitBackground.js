4a3a0be35cee4d731c88b1a954ccfe4b2efb3669
podamekalajagadeesh <podamekalajagadeesh@gmail.com>
2025-08-23 10:23:20 +0530

    Optimize performance: reduce animations, background elements, and improve rendering efficiency

--- src/CircuitBackground.js
+++ src/CircuitBackground.js
@@ -18,8 +18,6 @@ const CircuitBackground = () => {
         <div className="circuit-line"></div>
         <div className="circuit-line"></div>
         <div className="circuit-line"></div>
-        <div className="circuit-line"></div>
-        <div className="circuit-line"></div>
       </div>
       
       {/* Background React Symbols */}
@@ -41,18 +39,6 @@ const CircuitBackground = () => {
         <ellipse cx="50" cy="50" rx="30" ry="8" fill="none" stroke="var(--neon-cyan)" strokeWidth="2" transform="rotate(60 50 50)"/>
         <ellipse cx="50" cy="50" rx="30" ry="8" fill="none" stroke="var(--neon-cyan)" strokeWidth="2" transform="rotate(120 50 50)"/>
       </svg>
-      <svg className="bg-react-symbol react-4" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="50" cy="50" r="8" fill="var(--neon-cyan)"/>
-        <ellipse cx="50" cy="50" rx="30" ry="8" fill="none" stroke="var(--neon-cyan)" strokeWidth="2" transform="rotate(0 50 50)"/>
-        <ellipse cx="50" cy="50" rx="30" ry="8" fill="none" stroke="var(--neon-cyan)" strokeWidth="2" transform="rotate(60 50 50)"/>
-        <ellipse cx="50" cy="50" rx="30" ry="8" fill="none" stroke="var(--neon-cyan)" strokeWidth="2" transform="rotate(120 50 50)"/>
-      </svg>
-      <svg className="bg-react-symbol react-5" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="50" cy="50" r="8" fill="var(--neon-cyan)"/>
-        <ellipse cx="50" cy="50" rx="30" ry="8" fill="none" stroke="var(--neon-cyan)" strokeWidth="2" transform="rotate(0 50 50)"/>
-        <ellipse cx="50" cy="50" rx="30" ry="8" fill="none" stroke="var(--neon-cyan)" strokeWidth="2" transform="rotate(60 50 50)"/>
-        <ellipse cx="50" cy="50" rx="30" ry="8" fill="none" stroke="var(--neon-cyan)" strokeWidth="2" transform="rotate(120 50 50)"/>
-      </svg>
 
       {/* Background Ethereum Symbols */}
       <svg className="bg-ethereum-symbol eth-1" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
@@ -64,18 +50,6 @@ const CircuitBackground = () => {
         <polygon points="50,45 30,50 50,80 70,50" fill="var(--neon-blue)"/>
       </svg>
       <svg className="bg-ethereum-symbol eth-3" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-        <polygon points="50,20 30,50 50,45 70,50" fill="var(--neon-cyan)"/>
-        <polygon points="50,45 30,50 50,80 70,50" fill="var(--neon-blue)"/>
-      </svg>
-      <svg className="bg-ethereum-symbol eth-4" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-        <polygon points="50,20 30,50 50,45 70,50" fill="var(--neon-cyan)"/>
-        <polygon points="50,45 30,50 50,80 70,50" fill="var(--neon-blue)"/>
-      </svg>
-      <svg className="bg-ethereum-symbol eth-5" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-        <polygon points="50,20 30,50 50,45 70,50" fill="var(--neon-cyan)"/>
-        <polygon points="50,45 30,50 50,80 70,50" fill="var(--neon-blue)"/>
-      </svg>
-      <svg className="bg-ethereum-symbol eth-6" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
         <polygon points="50,20 30,50 50,45 70,50" fill="var(--neon-cyan)"/>
         <polygon points="50,45 30,50 50,80 70,50" fill="var(--neon-blue)"/>
       </svg>
